e24681f4e87cd183409662b13c11c970bcc3d862
Firmansyah Yanuar <fyfirman@gmail.com>
2021-12-15 23:07:54 +0700

    feat: add button request camera access

--- camera/camera.js
+++ camera/camera.js
@@ -1,5 +1,6 @@
 var cameraElements = document.getElementById('cameraOnWebview');
 var logger = document.getElementById('logger');
+var button = document.getElementById('button');
 var requestCameraPermissionAndroid;
 
 if (navigator.mediaDevices.getUserMedia) {
@@ -11,7 +12,7 @@ if (navigator.mediaDevices.getUserMedia) {
       cameraElements.setAttribute('playsinline', '');
     })
     .catch(function (e) {
-      logger.insertAdjacentHTML('beforeend',
+      logger.insertAdjacentHTML('afterbegin',
         `<span class="flex flex-col bg-red-50 p-2 rounded-md text-red-600 font-mono text-xs font-medium mb-2">
           Error (navigator.mediaDevices): ${e.message}
         </span>`
@@ -19,30 +20,41 @@ if (navigator.mediaDevices.getUserMedia) {
     })
 }
 
-if(!!requestCameraPermissionAndroid){
-  logger.insertAdjacentHTML('beforeend',
-    `<span class="flex flex-col bg-green-50 p-2 rounded-md text-green-600 font-mono text-xs font-medium mb-2">
-      Log: requestCameraPermissionAndroid is executed
-    </span>`
-  );
-  requestCameraPermissionAndroid();
-}else {
-  logger.insertAdjacentHTML('beforeend',
-    `<span class="flex flex-col bg-yellow-50 p-2 rounded-md text-yellow-600 font-mono text-xs font-medium mb-2">
-      Warn: requestCameraPermissionAndroid is not found
-    </span>`
-  );
+const request = async () => {
+  try {
+    await requestCameraPermissionAndroid();
+    logger.insertAdjacentHTML('afterbegin',
+      `<span class="flex flex-col bg-green-50 p-2 rounded-md text-green-600 font-mono text-xs font-medium mb-2">
+        Log: requestCameraPermissionAndroid is executed
+      </span>`
+    );
+  } catch (error) {
+    logger.insertAdjacentHTML('afterbegin',
+      `<span class="flex flex-col bg-yellow-50 p-2 rounded-md text-yellow-600 font-mono text-xs font-medium mb-2">
+        Warn: ${error.message}
+      </span>`
+    );
+  }
 }
+
+request();
+
+button.addEventListener('click', async () => {
+  try {
+    await request();
+  } catch (error) {
+  }
+});
 
 window.addEventListener('message', (message) => {
   if(message.data.error){
-    logger.insertAdjacentHTML('beforeend',
+    logger.insertAdjacentHTML('afterbegin',
       `<span class="flex flex-col bg-red-50 p-2 rounded-md text-red-600 font-mono text-xs font-medium mb-2">
         Error: ${message.data.error}
       </span>`
     );
   }else if(message.data.cameraPermission){
-    logger.insertAdjacentHTML('beforeend',
+    logger.insertAdjacentHTML('afterbegin',
       `<span class="flex flex-col bg-red-50 p-2 rounded-md text-red-600 font-mono text-xs font-medium mb-2">
         cameraPermission: ${message.data.cameraPermission}
       </span>`
